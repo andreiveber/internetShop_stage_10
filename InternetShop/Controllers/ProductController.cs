4e92e762a8b748278e3a66b55ee4a47fe10d2495
Veber_gs03@top-academy.ru <Veber_gs03@top-academy.ru>
2026-03-31 22:07:49 +0500

    Добавлена форма добавления товара с валидацией (клиентская и серверная)

---
+++ InternetShop/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InternetShop.Models;
+using InternetShop.Services;
+
+namespace InternetShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Добавление товара";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Товар успешно добавлен!";
+                return RedirectToAction("Products", "Home");
+            }
+
+            return View(product);
+        }
+    }
+}
--- InternetShop/Models/Product.cs
+++ InternetShop/Models/Product.cs
@@ -1,10 +1,25 @@
-﻿namespace InternetShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternetShop.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Название' обязательно для заполнения")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Название должно содержать от 2 до 100 символов")]
+        [Display(Name = "Название товара")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Поле 'Цена' обязательно для заполнения")]
+        [Range(0.01, 1000000, ErrorMessage = "Цена должна быть от 0.01 до 1 000 000 руб.")]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Описание' обязательно для заполнения")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Описание должно содержать от 10 до 500 символов")]
+        [Display(Name = "Описание товара")]
         public string Description { get; set; } = string.Empty;
     }
 }
